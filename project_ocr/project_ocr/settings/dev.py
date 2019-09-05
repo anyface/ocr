dc0b0dc96b8284c9db491ecf52e072b6c32f50f0
JieZhang <zhangjie.d@mininglamp.com>
2019-09-05 10:36:53 +0800

    配置mysql数据库 create database project_ocr

--- project_ocr/project_ocr/settings/dev.py
+++ project_ocr/project_ocr/settings/dev.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'project_ocr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project_ocr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_ocr',
+        'USER':'root',
+        'PASSWORD':'123',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
